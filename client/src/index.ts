4d2244b10e22ec0411ba3744328f0b9a0fd4cd1c
arayaryoma <r1o.ryoma.abe@gmail.com>
2020-06-17 03:02:28 +0900

    Send drawed data to the server and then the server send message to clients

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,16 +1,30 @@
-import './styles/main.css';
+import { v4 as uuid } from "uuid";
+import "./styles/main.css";
 const socket = new WebSocket("ws://localhost:3000");
 const $$ = document.querySelectorAll.bind(document);
 const $ = document.querySelector.bind(document);
+const userId = uuid();
 socket.onopen = (event) => {
   console.log("connected to the websocket server");
   socket.send("hello!!!");
 };
+type MessageData = {
+  event: string;
+};
+
+interface DrawedMessageData extends MessageData {
+  event: "drawed";
+  data: {
+    x: number;
+    y: number;
+  };
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = $("#canvas") as HTMLCanvasElement;
   const canvasManager = new CanvasManager(canvas);
   canvasManager.activate();
+  canvasManager;
 });
 
 class CanvasManager {
@@ -19,7 +33,7 @@ class CanvasManager {
   private ctx: CanvasRenderingContext2D;
   private canvasRect: DOMRect;
   private mousePoint = { x: 0, y: 0 };
-//   private buttonEl: HTMLButtonElement;
+  //   private buttonEl: HTMLButtonElement;
 
   constructor(canvasEl: HTMLCanvasElement) {
     this.canvas = canvasEl;
@@ -41,6 +55,15 @@ class CanvasManager {
         this.setMousePoint(ev);
         this.ctx.lineTo(this.mousePoint.x, this.mousePoint.y);
         this.ctx.stroke();
+        const data = {
+          event: "drawed",
+          point: {
+            x: this.mousePoint.x,
+            y: this.mousePoint.y,
+          },
+          userId,
+        };
+        socket.send(JSON.stringify(data));
       }
     });
     this.canvas.addEventListener("mouseup", (ev) => {
@@ -50,6 +73,23 @@ class CanvasManager {
     this.canvas.addEventListener("mouseleave", (ev) => {
       this.isDrawing = false;
     });
+
+    socket.onmessage = (ev) => {
+      try {
+          const json = JSON.parse(ev.data);
+          if(json.event==='drawed' && json.userId !== userId) {
+              const {x, y} = json.point;
+               const ctx = this.canvas.getContext('2d');
+               ctx?.beginPath();
+               ctx?.moveTo(0, 0);
+               ctx?.lineTo(x, y);
+               ctx?.stroke();
+               ctx?.closePath();
+          }
+      } catch (e) {
+        console.error(e);
+      }
+    };
     // this.buttonEl.addEventListener("click", (ev) => {
     //   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     // });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,25 @@
 import WebSocket from "ws";
+interface DrawedMessageData {
+  event: "drawed";
+  data: {
+    x: number;
+    y: number;
+  };
+}
 
 const server = new WebSocket.Server({
   port: 3000,
 });
+const sockets: WebSocket[] = [];
 server.on("connection", function (socket, req) {
+  sockets.push(socket);
   socket.on("message", (data) => {
-    console.log('message received:', data);
+    console.log(data);
+    try {
+      const json = JSON.parse(data.toString());
+      if (json.event === "drawed") {
+        sockets.forEach((s) => s.send(data));
+      }
+    } catch (e) {}
   });
-  socket.send("connected");
 });
